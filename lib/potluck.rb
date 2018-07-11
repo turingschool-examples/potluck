e387c9bab747595b6d0a92a9e5a8204efaedff92
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-07-11 14:38:39 -0600

    adds draft methods for Iteration 3

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -3,16 +3,33 @@ require 'pry'
 
 class Potluck
 
-  attr_reader :date,
-              :dishes
+  attr_accessor :date,
+                :dishes,
+                :name,
+                :category
 
   def initialize(date)
     @date = date
+    @dish = Dish.new(name, category)
+    @name = name
+    @category = category
     @dishes = []
+    @dishes_by_category = Hash.new([])
+    #no default value needed by above
   end
 
   def add_dish(dish)
     @dishes << dish
+
+    if @dishes_by_category[dish.category]
+      @dishes_by_category[dish.category].push(dish)
+    else
+      @dishes_by_category[dish.category] = dish
+    end
+  end
+
+  def get_all_from_category(category)
+    @dishes_by_category[category]
   end
 
 end
--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -26,4 +26,24 @@ class DishTest < Minitest::Test
     assert_equal 2, potluck.dishes.length
   end
 
+  def test_it_returns_dishes_by_category
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    # cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    # candy_salad = Dish.new("Candy Salad", :dessert)
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    # potluck.add_dish(cocktail_meatballs)
+    # potluck.add_dish(candy_salad)
+    actual = potluck.get_all_from_category(:appetizer)
+    expected = [couscous_salad, summer_pizza]
+    assert_equal expected, actual
+
+    # potluck.get_all_from_category(:appetizer).first
+    # potluck.get_all_from_category(:appetizer).first.name
+  end
+
 end
