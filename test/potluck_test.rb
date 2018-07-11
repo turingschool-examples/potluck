40835c1b227fd342497b51e2510ba7abd4a152c2
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-07-11 14:02:35 -0600

    Adds test for menu method

--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -67,4 +67,29 @@ class PotluckTest < Minitest::Test
     potluck.add_dish(candy_salad)
     assert_equal [couscous_salad, summer_pizza], potluck.get_all_from_category(:appetizer)
   end
+
+  def test_it_can_return_menu
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    bean_dip = Dish.new("Bean Dip", :appetizer)
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+    potluck.add_dish(bean_dip)
+    expected = {
+      appetizer: ["Bean Dip", "Couscous Salad", "Summer Pizza"],
+      entres: ["Cocktail Meatballs", "Roast Pork"],
+      desserts: ["Candy Salad"]
+    }
+
+    assert_equal expected, potluck.menu
+
+
+  end
 end
