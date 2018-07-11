089748fde093b42c34378d269b5fe1541d954090
Tara Craig <tara.craig7@gmail.com>
2018-07-11 15:06:20 -0600

    Finish menu, start ratio.

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -20,5 +20,59 @@ class Potluck
     end
   end
 
+  def menu_hash(appetizer_names, entre_names, dessert_names)
+    {:appetizers => appetizer_names,
+      :entres => entre_names,
+      :desserts => dessert_names
+    }
+  end
 
+  def menu
+    dishes = @dishes
+    appetizer_names = apps(dishes)
+    entre_names = entres(dishes)
+    dessert_names = desserts(dishes)
+    menu_hash(appetizer_names, entre_names, dessert_names)
+  end
+
+  def apps(dishes)
+    app_names = []
+    dishes.find_all do |dish|
+      if dish.category == :appetizer
+        app_names << dish.name
+      else
+        dish.name
+      end
+    end
+    app_names.sort
+  end
+
+  def entres(dishes)
+    entre_names = []
+    dishes.find_all do |dish|
+      if dish.category == :entre
+        entre_names << dish.name
+      else
+        dish.name
+      end
+    end
+    entre_names.sort
+  end
+
+  def desserts(dishes)
+    dessert_names = []
+    dishes.find_all do |dish|
+      if dish.category == :dessert
+        dessert_names << dish.name
+      else
+        dish.name
+      end
+    end
+    dessert_names.sort
+  end
+
+  def ratio()
+    amount
+
+  end
 end
--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -48,4 +48,67 @@ class PotluckTest < Minitest::Test
     assert_equal "Couscous Salad", potluck.get_all_from_category(:appetizer).first.name
   end
 
+  def test_it_can_pull_appetizer_names
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    bean_dip = Dish.new("Bean Dip", :appetizer)
+
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+    potluck.add_dish(bean_dip)
+
+    input = [couscous_salad, summer_pizza, bean_dip, candy_salad, roast_pork, cocktail_meatballs]
+
+    expected = ["Bean Dip", "Couscous Salad", "Summer Pizza"]
+
+    assert_equal expected, potluck.apps(input)
+  end
+
+  def test_it_can_show_menu
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    bean_dip = Dish.new("Bean Dip", :appetizer)
+
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+    potluck.add_dish(bean_dip)
+
+    expected = {:appetizers=>["Bean Dip", "Couscous Salad", "Summer Pizza"],:entres=>["Cocktail Meatballs", "Roast Pork"],:desserts=>["Candy Salad"]}
+
+    assert_equal expected, potluck.menu
+  end
+
+  def test_it_can_find_ratio
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    bean_dip = Dish.new("Bean Dip", :appetizer)
+
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+    potluck.add_dish(bean_dip)
+
+    assert_equal 50.0, potluck.ratio(:appetizer)
+  end
+
 end
