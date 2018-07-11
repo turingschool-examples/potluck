7bcd2c485b2780cd44af425499f6ccc698a9b439
andrewetobin <andrewetobin@gmail.com>
2018-07-11 13:30:14 -0600

    finish iteration 2

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -1,9 +1,17 @@
 class Potluck
-  attr_reader :date
+  attr_reader :date, :dishes
 
   def initialize(date)
     @date = date
+    @dishes = []
   end
+
+  def add_dish(dish_instance)
+    @dishes << dish_instance
+
+  end
+
+
 
 
 
--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -7,30 +7,32 @@ class PotluckTest < Minitest::Test
   def test_it_exists
     potluck = Potluck.new("7-13-18")
 
-    assert_instance_of Potluck, potluck 
+    assert_instance_of Potluck, potluck
+  end
+
+  def test_it_has_attributes
+    potluck = Potluck.new("7-13-18")
+
+    assert_equal "7-13-18", potluck.date
+  end
+
+  def test_it_starts_with_zero_dishes
+    potluck = Potluck.new("7-13-18")
+
+    assert_equal [], potluck.dishes
+  end
+
+  def test_add_dishes
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(cocktail_meatballs)
+
+    expected = [couscous_salad, cocktail_meatballs]
+    assert_equal expected, potluck.dishes
+    assert_equal 2, potluck.dishes.length
   end
 
 
 end
-# pry(main)> require './lib/dish'
-# #=> true
-# pry(main)> require './lib/potluck'
-# #=> true
-# pry(main)> potluck = Potluck.new("7-13-18")
-# #=> #<Potluck:0x00007fccc4abe940...>
-# pry(main)> potluck.date
-# #=> "7-13-18"
-# pry(main)> potluck.dishes
-# #=> []
-# pry(main)> couscous_salad = Dish.new("Couscous Salad", :appetizer)
-# #=> #<Dish:0x00007fccc4249940...>
-# pry(main)> cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
-# #=> #<Dish:0x00007fccc499ceb8...>
-# pry(main)> potluck.add_dish(couscous_salad)
-# #=> [#<Dish:0x00007fccc4249940...>]
-# pry(main)> potluck.add_dish(cocktail_meatballs)
-# #=> [#<Dish:0x00007fccc4249940...>, #<Dish:0x00007fccc499ceb8...>]
-# pry(main)> potluck.dishes
-# #=> [#<Dish:0x00007fccc4249940...>, #<Dish:0x00007fccc499ceb8...>]
-# pry(main)> potluck.dishes.length
-# #=> 2
