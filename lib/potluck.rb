379e859f82b5a10307da04141b46e058349792ea
Harper Bellows <harper.bellows@gmail.com>
2018-07-11 13:59:47 -0600

    add add_dish method

---
+++ lib/potluck.rb
@@ -0,0 +1,21 @@
+class Potluck
+  attr_reader :date,
+              :dishes
+
+  def initialize(date)
+    @date = date
+    @dishes = []
+  end
+
+  def add_dish(dish)
+    @dishes << dish
+  end
+
+  def get_all_from_category(category)
+    @dishes.map do |dish|
+      if dish.category == category
+        dish
+      end
+    end
+  end
+end
