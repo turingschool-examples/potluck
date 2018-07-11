3a2f5fcbaa5c8387b17338504422eb3b28c63064
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-11 13:54:04 -0600

    test it has a date

--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -6,6 +6,11 @@ require "./lib/potluck"
 class PotluckTest < Minitest::Test
   def test_it_exists
     potluck = Potluck.new("7-13-18")
-    assert_instance_of Potluck, potluck 
+    assert_instance_of Potluck, potluck
+  end
+
+  def test_it_has_a_date
+    potluck = Potluck.new("7-13-18")
+    assert_equal "7-13-18", potluck.date
   end
 end
