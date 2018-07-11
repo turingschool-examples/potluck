4145ab987ec50152b3598e2e7572ba3026e111a9
T Fielder <fiel1139@hotmail.com>
2018-07-11 13:40:50 -0600

    adds implementation for get_all_from_category.

--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -39,5 +39,21 @@ class PotluckTest < Minitest::Test
     assert_equal 2, potluck.dishes.length
   end
 
+  def test_it_gets_all_from_category
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+    assert_equal [couscous_salad, summer_pizza], potluck.get_all_from_category(:appetizer)
+    assert_equal [roast_pork, cocktail_meatballs], potluck.get_all_from_category(:entre)
+    assert_equal [candy_salad], potluck.get_all_from_category(:dessert)
+  end
 
 end
