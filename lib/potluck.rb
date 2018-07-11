9cd399c56fa7a34b2ca91e3e4d0a2fa45b28aff8
Ronald Huston Jr <rghuston9@gmail.com>
2018-07-11 14:54:37 -0600

    some iteration 3 functionality

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -7,7 +7,7 @@ class Potluck
   def initialize(date)
     @date = date
     @dishes = []
-
+    @category = {appetizer: "", entre: "", dessert: ""}
   end
 
   def date
@@ -16,10 +16,22 @@ class Potluck
 
   def add_dish(name)
     @dishes << Dish.new(name, category)
+    @dishes.add_to_category
   end
 
   def dishes
     @dishes
+  end
+
+  def add_to_category
+    dishes.each do |name|
+      @category[category] << dish.name
+    end
+    dish.category
+  end
+
+  def get_all_from_category(:category)
+    @category[:category]
   end
 
 end
