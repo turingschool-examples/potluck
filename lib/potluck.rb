954f8c5c0e760541fc397177e5a450c6f88cb887
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-07-11 13:50:49 -0600

    Adds get_all_from_category method

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -12,4 +12,10 @@ class Potluck
   def add_dish(dish)
     @dishes << dish
   end
+
+  def get_all_from_category(category)
+    dishes_by_category = @dishes.find_all do |dish|
+      dish.category == category
+    end
+  end
 end
