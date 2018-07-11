3e06ff1a5874190bee6f1236f7ed0ffd65dbe099
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-11 13:22:17 -0600

    Adds Potluck class methods and tests

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -1,6 +1,12 @@
 class Potluck
+  attr_reader :date, :dishes
 
   def initialize(date)
-  end 
+    @date = date
+    @dishes = []
+  end
 
+ def add_dish(new_dish)
+   @dishes << new_dish
+ end
 end
--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -9,6 +9,32 @@ class PotluckTest < Minitest::Test
     potluck = Potluck.new("7-13-18")
 
     assert_instance_of Potluck, potluck
-  end 
+  end
+
+  def test_it_has_a_date
+    potluck = Potluck.new("7-13-18")
+
+    assert_equal "7-13-18", potluck.date
+  end
+
+  def test_it_starts_with_no_dishes
+    potluck = Potluck.new("7-13-18")
+
+    assert_equal [], potluck.dishes
+  end
+
+  def test_can_add_dishes
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    potluck = Potluck.new("7-13-18")
+
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(cocktail_meatballs)
+    actual = potluck.dishes
+    expected = [couscous_salad, cocktail_meatballs]
+
+    assert_equal expected, actual
+    assert_equal 2, potluck.dishes.length
+  end
 
 end
