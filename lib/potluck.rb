27ad1359484da3323d14ab95e3e0238bc6846532
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-11 14:53:18 -0600

    Adds functionality and tests for Iteration 4

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -3,15 +3,42 @@ class Potluck
 
   def initialize(date)
     @date = date
-    @dishes = {}
+    @dishes = []
   end
 
- def add_dish(new_dish)
-   @dishes[new_dish.category] = new_dish.name
- end
+  def add_dish(new_dish)
+    @dishes << new_dish
+  end
 
  def get_all_from_category(category)
-   @dishes[category]
+   @dishes.find_all do |dish|
+     if dish.category == category
+       dish
+     end
+   end
  end
 
+ def menu
+  menu_array = {appetizers: [], entres: [], desserts: []}
+  @dishes.each do |dish|
+    if dish.category == :appetizer
+      menu_array[:appetizers] << dish.name
+      menu_array[:appetizers] = menu_array[:appetizers].sort
+    elsif dish.category == :entre
+      menu_array[:entres] << dish.name
+      menu_array[:entres] = menu_array[:entres].sort
+    else
+      menu_array[:desserts] << dish.name
+      menu_array[:desserts] = menu_array[:desserts].sort
+    end
+  end
+  menu_array
+end
+
+  def ratio(category)
+    menu_array = menu
+    total_items = menu_array.values.flatten.count
+    category_items = menu_array[category].count
+    category_items.to_f / total_items
+  end
 end
--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -19,7 +19,7 @@ class PotluckTest < Minitest::Test
 
   def test_it_starts_with_no_dishes
     potluck = Potluck.new("7-13-18")
-    expected = {}
+    expected = []
 
     assert_equal expected, potluck.dishes
   end
@@ -32,14 +32,13 @@ class PotluckTest < Minitest::Test
     potluck.add_dish(couscous_salad)
     potluck.add_dish(cocktail_meatballs)
     actual = potluck.dishes
-    expected = {appetizer: "Couscous Salad", entre: "Cocktail Meatballs"}
+    expected = [couscous_salad, cocktail_meatballs]
 
     assert_equal expected, actual
     assert_equal 2, potluck.dishes.length
   end
 
   def test_can_list_dishes_by_category
-    skip
     couscous_salad = Dish.new("Couscous Salad", :appetizer)
     cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
     summer_pizza = Dish.new("Summer Pizza", :appetizer)
@@ -57,6 +56,53 @@ class PotluckTest < Minitest::Test
     actual = potluck.get_all_from_category(:appetizer)
 
     assert_equal expected, actual
+    assert_equal couscous_salad, actual.first
     assert_equal "Couscous Salad", actual.first.name
+  end
+
+  def test_menu_lists_dishs_in_hash
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    bean_dip = Dish.new("Bean Dip", :appetizer)
+    potluck = Potluck.new("7-13-18")
+
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(candy_salad)
+    potluck.add_dish(bean_dip)
+
+    expected = {:appetizers=>["Bean Dip", "Couscous Salad", "Summer Pizza"],
+                :entres=>["Cocktail Meatballs", "Roast Pork"],
+                :desserts=>["Candy Salad"]}
+    actual = potluck.menu
+
+    assert_equal expected, actual
+  end
+
+  def test_it_gives_ratio_of_items_by_category
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    bean_dip = Dish.new("Bean Dip", :appetizer)
+    potluck = Potluck.new("7-13-18")
+
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(candy_salad)
+    potluck.add_dish(bean_dip)
+
+    expected = 0.50
+    actual = potluck.ratio(:appetizers)
+
+    assert_equal expected, actual
   end
 end
