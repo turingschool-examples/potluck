630a9b4f0ce37ddce6461400db3b33641c9d004d
Mike McKee <mikecm@gmail.com>
2018-07-11 13:12:15 -0600

    Finishing dish class interaction pattern/tests

--- lib/dish.rb
+++ lib/dish.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
+# Dish Class
 class Dish
-  def initialize
+  attr_reader :name,
+              :category
+
+  def initialize(name, category)
+    @name = name
+    @category = category
   end
 end
--- test/dish_test.rb
+++ test/dish_test.rb
@@ -6,8 +6,24 @@ require './lib/dish'
 
 class DishTest < Minitest::Test
   def test_it_exists
-    dish = Dish.new
+    dish = Dish.new('Couscous Salad', :appetizer)
 
     assert_instance_of Dish, dish
+  end
+
+  def test_it_has_a_name
+    dish = Dish.new('Couscous Salad', :appetizer)
+    expected = 'Couscous Salad'
+    actual = dish.name
+
+    assert_equal expected, actual
+  end
+
+  def test_it_has_a_category
+    dish = Dish.new('Couscous Salad', :appetizer)
+    expected = :appetizer
+    actual = dish.category
+
+    assert_equal expected, actual
   end
 end
