56f6c5352c048bfb0198b866acb49ec9d21fd27c
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-11 13:42:23 -0600

    add an initialize method for name

--- lib/dish.rb
+++ lib/dish.rb
@@ -2,4 +2,17 @@ require "pry"
 
 class Dish
 
-end 
+  def initialize(name, category)
+    @name = name
+    @category = category
+    # will probably need a hash
+    # dishes = {:appetizer, "Couscous Salad"}
+  end
+
+  # will need a method that calls the dishes name 
+  def name
+    @name
+  end
+
+
+end
