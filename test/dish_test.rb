e0f0ca9b695db72983e0b686e5cc8b6663b6f1a8
T Fielder <fiel1139@hotmail.com>
2018-07-11 13:16:21 -0600

    implementation of two tests it has a name and it has a category.

--- test/dish_test.rb
+++ test/dish_test.rb
@@ -3,7 +3,22 @@ require "autorun/pride"
 require "./lib/dish.rb"
 
 class DishTest < Minitest::Test
+
+  def test_it_exists
+  dish = Dish.new("Couscous Salad", :appetizer)
+  assert_instance_of Dish, dish
+  end
+
+  def test_it_has_name
   dish = Dish.new("Couscous Salad", :appetizer)
 
-  assert_instance_of Dish, dish
+  assert_equal "Couscous Salad", dish.name
+  end
+
+  def test_it_has_category
+    dish = Dish.new("Couscous Salad", :appetizer)
+
+    assert_equal :appetizer, dish.category
+  end
+
 end
