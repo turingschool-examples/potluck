107e38c09a615d0048485714ee8ee5ff10de4066
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-11 14:10:20 -0600

    add test for if it has couscous or meatballs

--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -19,4 +19,23 @@ class PotluckTest < Minitest::Test
     assert_equal [], potluck.dishes
   end
 
+  def test_it_has_couscous
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    assert_instance_of Dish, couscous_salad
+  end
+
+  def test_it_has_meatballs
+    potluck = Potluck.new("7-13-18")
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entree)
+    assert_instance_of Dish, cocktail_meatballs
+  end
+  # def test_adding_dishes_to_the_potluck
+  #   potluck = Potluck.new("7-13-18")
+  #   couscous_salad = Dish.new("Couscous Salad", :appetizer)
+  #   cocktail_meatballs = Dish.new("Cocktail Meatballs", :entree)
+  #   potluck.add_dish(couscous_salad)
+  #   potluck.add_dish(cocktail_meatballs)
+  # end
+
 end
