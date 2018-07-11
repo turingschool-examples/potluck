b3154fb823bdb0f7fafc08a6dbab4d740ad9f766
Tom Nunez <tomjnunez@gmail.com>
2018-07-11 14:52:59 -0600

    finished first and second iteration, started on the third

---
+++ test/potluck_dish_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/dish'
+
+class DishTest < Minitest::Test
+  def test_it_exists
+    dish = Dish.new("Couscous Salad", :appetizer)
+
+    assert_instance_of Dish, dish
+  end
+
+  def test_it_has_name
+    dish = Dish.new("Couscous Salad", :appetizer)
+
+    assert_equal "Couscous Salad", dish.name
+  end
+
+  def test_it_has_a_category
+    dish = Dish.new("Couscous Salad", :appetizer)
+
+    assert_equal :appetizer, dish.category
+  end
+
+end
