085ef85f734c832af94f8cbff465f8a5d963f326
T Fielder <fiel1139@hotmail.com>
2018-07-11 13:52:24 -0600

    tests for getting the first name of the dish in potluck.

--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -73,4 +73,23 @@ class PotluckTest < Minitest::Test
     assert_equal roast_pork, potluck.get_all_from_category(:entre).first
     assert_equal candy_salad, potluck.get_all_from_category(:dessert).first
   end
+
+  def test_it_returns_name_of_first_item_in_category
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+
+    assert_equal "Couscous Salad", potluck.get_all_from_category(:appetizer).first.name
+    assert_equal "Roast Pork", potluck.get_all_from_category(:entre).first.name
+    assert_equal "Candy Salad", potluck.get_all_from_category(:dessert).first.name
+  end
+
 end
