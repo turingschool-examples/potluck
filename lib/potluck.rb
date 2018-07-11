46b9fe814e48cdd121c5e60c3f9114aa19fe4396
KellyMarcilliat <kelly.marcilliat@gmail.com>
2018-07-11 14:19:59 -0600

    WIP - Complete on Iteration 3

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -12,6 +12,14 @@ class Potluck
   
   def add_dish(dish_object)
     @dishes << dish_object 
+    return @dishes 
   end 
+  
+  def get_all_from_category(category)
+    @dishes.find_all do |dish|
+      dish.category == category 
+    end 
+  end 
+        
   
 end 
--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -29,6 +29,53 @@ class PotluckTest < Minitest::Test
     assert_equal [couscous_salad, cocktail_meatballs], potluck.dishes 
     assert_equal 2, potluck.dishes.count
   end 
+  
+  def test_it_returns_updated_dishes_array
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    
+    assert_equal [couscous_salad], potluck.add_dish(couscous_salad)
+    
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+    
+    assert_equal [couscous_salad, summer_pizza, roast_pork, cocktail_meatballs, candy_salad],potluck.dishes  
+  end 
+  
+  def test_it_gets_all_from_category
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+    
+    assert_equal [couscous_salad, summer_pizza], potluck.get_all_from_category(:appetizer)
+    assert_equal couscous_salad, potluck.get_all_from_category(:appetizer).first 
+    assert_equal "Couscous Salad", potluck.get_all_from_category(:appetizer).first.name 
+    
+  end 
+  
+
+  # pry(main)> potluck.get_all_from_category(:appetizer)
+  # #=> [#<Dish:0x00007f942191e9b8...>, #<Dish:0x00007f9421d26880...>
+  # pry(main)> potluck.get_all_from_category(:appetizer).first
+  # #=> #<Dish:0x00007f942191e9b8...>
+  # pry(main)> potluck.get_all_from_category(:appetizer).first.name
+  # => "Couscous Salad"
+  # ```
     
 end 
 
