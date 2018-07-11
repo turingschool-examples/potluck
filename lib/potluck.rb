275ddc0577cc615297a6d0514bdb04cf04d141a9
andrewetobin <andrewetobin@gmail.com>
2018-07-11 13:42:50 -0600

    finished iteration 3

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -8,7 +8,12 @@ class Potluck
 
   def add_dish(dish_instance)
     @dishes << dish_instance
+  end
 
+  def get_all_from_category(category)
+    @dishes.select do |dish|
+      dish.category == category
+    end
   end
 
 
