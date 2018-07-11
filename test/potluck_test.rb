08b8b0d6d69793edfeb0fe94a62eccf65fb02826
T Fielder <fiel1139@hotmail.com>
2018-07-11 13:20:53 -0600

    adds the initialize test for potluck.

--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -4,7 +4,10 @@ require "./lib/dish.rb"
 require "./lib/potluck.rb"
 
 class PotluckTest < Minitest::Test
-
+  def test_it_exists
+  potluck = Potluck.new("7-13-18")
+  assert_instance_of Potluck, potluck
+  end
 
 
 end
