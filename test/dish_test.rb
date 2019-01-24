d8714435171f7a92df61a17f471089dfeee5b571
Matt Levy <milevy1@gmail.com>
2019-01-23 21:34:38 -0700

    Added more tests

--- test/dish_test.rb
+++ test/dish_test.rb
@@ -10,4 +10,16 @@ class DishTest < Minitest::Test
     assert_instance_of Dish, dish
   end
 
+  def test_dish_has_name
+    dish = Dish.new("Couscous Salad", :appetizer)
+
+    assert_equal "Couscous Salad", dish.name
+  end
+
+  def test_dish_has_category
+    dish = Dish.new("Couscous Salad", :appetizer)
+
+    assert_equal :appetizer, dish.category
+  end
+
 end
