76876eb4a163be86c56e26bd2905a99df2228ca9
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-03-13 17:50:15 -0600

    iteration 1 done

---
+++ test/dish_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/dish'
+
+class DishTest < Minitest::Test
+
+  def test_it_exists
+    dish = Dish.new("Couscous Salad", :appetizer)
+    assert_instance_of Dish, dish
+  end
+
+  def test_it_has_attributes
+    dish = Dish.new("Couscous Salad", :appetizer)
+    assert_equal "Couscous Salad", dish.name
+    assert_equal :appetizer, dish.category
+  end 
+end
