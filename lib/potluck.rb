8675b801f11baa2a06a0775b10ba72b10441a802
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-03-13 20:03:45 -0600

    iteration 3 done

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -8,5 +8,11 @@ class Potluck
 
   def add_dish(dish)
     @dishes << dish
-  end 
+  end
+
+  def get_all_from_category(category)
+    @dishes.find_all do |dish|
+      dish.category == category
+    end 
+  end
 end
--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -36,5 +36,70 @@ class PotluckTest < Minitest::Test
     potluck.add_dish(couscous_salad)
     potluck.add_dish(cocktail_meatballs)
     assert_equal 2, potluck.dishes.length
-  end 
+  end
+
+  def test_it_returns_dishes_in_category
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entree)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    roast_pork = Dish.new("Roast Pork", :entree)
+    potluck = Potluck.new("7-13-18")
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(candy_salad)
+    assert_equal [couscous_salad, summer_pizza], potluck.get_all_from_category(:appetizer)
+    assert_equal couscous_salad, potluck.get_all_from_category(:appetizer).first
+    assert_equal "Couscous Salad", potluck.get_all_from_category(:appetizer).first.name
+  end
 end
+
+**Note** for the the `menu` method, dishes are sorted alphabetically.
+
+
+pry(main)> potluck = Potluck.new("7-13-18")
+#=> #<Potluck:0x00007f9422838908...>
+
+pry(main)> couscous_salad = Dish.new("Couscous Salad", :appetizer)
+#=> #<Dish:0x00007f942191e9b8...
+
+pry(main)> summer_pizza = Dish.new("Summer Pizza", :appetizer)
+#=> #<Dish:0x00007f9421d26880...>
+
+pry(main)> roast_pork = Dish.new("Roast Pork", :entre)
+#=> #<Dish:0x00007f9421d04870...>
+
+pry(main)> cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+#=> #<Dish:0x00007f9421ce6e88...>
+
+pry(main)> candy_salad = Dish.new("Candy Salad", :dessert)
+#=> #<Dish:0x00007f9421cb60f8...>
+
+pry(main)> bean_dip = Dish.new("Bean Dip", :appetizer)
+#=> #<Dish:0x00007fa115885500...>
+
+pry(main)> potluck.add_dish(couscous_salad)
+#=> [#<Dish:0x00007f942191e9b8...]
+
+pry(main)> potluck.add_dish(summer_pizza)
+#=> [#<Dish:0x00007f942191e9b8...>, #<Dish:0x00007f9421d26880...>]
+
+pry(main)> potluck.add_dish(roast_pork)
+#=> [#<Dish:0x00007f942191e9b8...>, #<Dish:0x00007f9421d26880...>, #<Dish:0x00007f9421e26800...>]
+
+pry(main)> potluck.add_dish(cocktail_meatballs)
+#=> [#<Dish:0x00007f942191e9b8...>, #<Dish:0x00007f9421d26880...>, #<Dish:0x00007f9421e26800...>, #<Dish:0x00007f9421dAA770...>]
+
+pry(main)> potluck.add_dish(candy_salad)
+#=> [#<Dish:0x00007f942191e9b8...>, #<Dish:0x00007f9421d26880...>, #<Dish:0x00007f9421e26800...>, #<Dish:0x00007f9421dAA770...>, #<Dish:0x00007f9421dAA610...>]
+
+pry(main)> potluck.add_dish(bean_dip)
+#=> [#<Dish:0x00007f942191e9b8...>, #<Dish:0x00007f9421d26880...>, #<Dish:0x00007f9421e26800...>, #<Dish:0x00007f9421dAA770...>, #<Dish:0x00007f9421dAA610...>, #<Dish:0x00007f9421dAA680...>]
+
+pry(main)> potluck.menu
+# => {:appetizers=>["Bean Dip", "Couscous Salad", "Summer Pizza"],:entres=>["Cocktail Meatballs", "Roast Pork"],:desserts=>["Candy Salad"]}
+
+pry(main)> potluck.ratio(:appetizer)
+#=> 50.0
