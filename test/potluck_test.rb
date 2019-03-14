91bf6f90305e5e7eb801141a492499bf50efcddc
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-03-13 18:13:11 -0600

    Iteration 2 done

---
+++ test/potluck_test.rb
@@ -0,0 +1,40 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/dish'
+require './lib/potluck'
+
+class PotluckTest < Minitest::Test
+
+  def test_it_exists
+    potluck = Potluck.new("7-13-18")
+    assert_instance_of Potluck, potluck
+  end
+
+  def test_it_has_a_date
+    potluck = Potluck.new("7-13-18")
+    assert_equal "7-13-18", potluck.date
+  end
+
+  def test_it_starts_without_dishes
+    potluck = Potluck.new("7-13-18")
+    assert_equal [], potluck.dishes
+  end
+
+  def test_it_adds_dishes_to_potluck
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entree)
+    potluck = Potluck.new("7-13-18")
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(cocktail_meatballs)
+    assert_equal [couscous_salad, cocktail_meatballs], potluck.dishes
+  end
+
+  def test_it_returns_total_dishes_at_potluck
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entree)
+    potluck = Potluck.new("7-13-18")
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(cocktail_meatballs)
+    assert_equal 2, potluck.dishes.length
+  end 
+end
