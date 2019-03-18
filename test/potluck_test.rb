9bfe7b1e1420846ed9b745401dd383f5f2cabedb
Alexandra Chakeres <alexchakeres@gmail.com>
2019-03-18 11:23:46 -0600

    Add Potluck ability to filter dishes by category

--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -50,20 +50,56 @@ class PotluckTest < Minitest::Test
     assert_equal [couscous_salad, cocktail_meatballs], potluck.add_dish(cocktail_meatballs)
   end
 
-  # def test_it_gets_all_from_a_category
-  #   potluck = Potluck.new("7-13-18")
-  #   couscous_salad = Dish.new("Couscous Salad", :appetizer)
-  #   summer_pizza = Dish.new("Summer Pizza", :appetizer)
-  #   roast_pork = Dish.new("Roast Pork", :entre)
-  #   cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
-  #   candy_salad = Dish.new("Candy Salad", :dessert)
-  #
-  #   potluck.add_dish(couscous_salad)
-  #   potluck.add_dish(summer_pizza)
-  #   potluck.add_dish(roast_pork)
-  #   potluck.add_dish(cocktail_meatballs)
-  #   potluck.add_dish(candy_salad)
-  #
-  #   assert_equal [couscous_salad, summer_pizza], potluck.get_all_from_category
-  # end
+  def test_it_gets_all_from_a_category
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+
+    assert_equal [couscous_salad, summer_pizza], potluck.get_all_from_category(:appetizer)
+    assert_equal couscous_salad, potluck.get_all_from_category(:appetizer).first
+    assert_equal "Couscous Salad", potluck.get_all_from_category(:appetizer).first.name
+  end
+
+  def test_it_gets_all_from_a_different_category
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+
+    assert_equal [roast_pork, cocktail_meatballs], potluck.get_all_from_category(:entre)
+  end
+
+  def test_get_all_from_a_nonexistant_category_returns_empty_array
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+
+    assert_equal [], potluck.get_all_from_category(:breakfast)
+  end
 end
