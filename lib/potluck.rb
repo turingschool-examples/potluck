9b49037555873ecb365c8a91f8a2980eb78b0183
Alexandra Chakeres <alexchakeres@gmail.com>
2019-03-18 10:55:38 -0600

    Add Potluck and PotluckTest classes with initialization

---
+++ lib/potluck.rb
@@ -0,0 +1,7 @@
+class Potluck
+  attr_reader :date
+
+  def initialize(date)
+    @date = date
+  end
+end
---
+++ test/potluck_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/dish'
+require './lib/potluck'
+
+class PotluckTest < Minitest::Test
+  def test_it_exists
+    potluck = Potluck.new("7-13-18")
+
+    assert_instance_of Potluck, potluck
+  end
+
+  def test_it_has_a_date
+    potluck = Potluck.new("7-13-18")
+
+    assert_equal "7-13-18", potluck.date
+  end
+
+  def test_it_has_a_different_date
+    potluck = Potluck.new("5-4-19")
+
+    assert_equal "5-4-19", potluck.date
+  end
+end
