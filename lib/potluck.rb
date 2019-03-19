6f334a077c790576ec98511b99896f873d902ab2
Alexandra Chakeres <alexchakeres@gmail.com>
2019-03-18 21:59:38 -0600

    Add s to end of categories in Potluck menu

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -23,21 +23,31 @@ class Potluck
     end
   end
 
+  # move to a new class?
   def menu_generator
     @menu = Hash.new([])
 
+    # move to another method
     @dishes.each do |dish|
 
-      if !@menu.has_key?(dish.category) # add s
-        @menu[dish.category] = [] # add s
+      key = add_s(dish.category)
+
+      if !@menu.has_key?(key)
+        @menu[key] = []
       end
 
-      @menu[dish.category] << dish.name # add s
+      @menu[key] << dish.name
     end
 
+    # move to another method
     @menu.each_key do |category|
       @menu[category].sort!
     end
 
   end
+
+  def add_s(symbol)
+    (symbol.to_s + "s").to_sym
+  end
+
 end
