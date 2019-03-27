77556d9dc7efa7647bb2d272097de61dfdac78cd
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-03-27 10:49:58 -0600

    Add Potluck menu Ratio method tests

--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -93,4 +93,16 @@ class PotluckTest < Minitest::Test
     test_menu = {:appetizers=>["Bean Dip", "Couscous Salad", "Summer Pizza"],:entrees=>["Cocktail Meatballs", "Roast Pork"],:desserts=>["Candy Salad"]}
     assert_equal test_menu, @potluck.menu
   end
+
+  def test_it_can_return_the_correct_ratio_of_dishes
+    @potluck.add_dish(@bean_dip)
+    @potluck.add_dish(@couscous_salad)
+    @potluck.add_dish(@cocktail_meatballs)
+    @potluck.add_dish(@summer_pizza)
+    @potluck.add_dish(@roast_pork)
+    @potluck.add_dish(@candy_salad)
+
+    assert_equal 50.0, potluck.ratio(:appetizer)
+  end
+
 end
