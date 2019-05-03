447113c42404dcbbc3ba69db65abb0df3170c889
Jake Miller <jake0miller@gmail.com>
2019-05-03 11:47:42 -0600

    Finish potluck and tests

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -4,9 +4,30 @@ class Potluck
   def initialize(date)
     @date = date
     @dishes = []
+    @menu = {}
+    @num_dishes = 0
   end
 
   def add_dish(dish)
     @dishes << dish
+    cat = dish.category.to_s
+    cat += 's'
+    cat = cat.to_sym
+    @menu[cat] ||= []
+    @menu[cat] << dish.name
+    @menu[cat].sort!
+    @num_dishes += 1
+  end
+
+  def get_all_from_category(category)
+    @dishes.find_all { |dish| dish.category == category }
+  end
+
+  def menu
+    @menu
+  end
+
+  def ratio(category)
+    (100 * get_all_from_category(category).length.to_f / @num_dishes).round(1)
   end
 end
--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -7,7 +7,25 @@ class CardTest < Minitest::Test
   def setup
     @potluck = Potluck.new("7-13-18")
     @salad = Dish.new("Couscous Salad", :appetizer)
+    @pizza = Dish.new("Summer Pizza", :appetizer)
     @meatballs = Dish.new("Cocktail Meatballs", :entre)
+    @pork = Dish.new("Roast Pork", :entre)
+    @dessert = Dish.new("Candy Salad", :dessert)
+    @dip = Dish.new("Bean Dip", :appetizer)
+  end
+
+  def test_menu_is_alphabetical
+    @potluck.add_dish(@salad)
+    @potluck.add_dish(@pizza)
+    @potluck.add_dish(@pork)
+    @potluck.add_dish(@meatballs)
+    @potluck.add_dish(@dessert)
+    @potluck.add_dish(@dip)
+    assert_equal 6, @potluck.dishes.length
+    assert_equal 3, @potluck.menu.length
+    assert_equal [@dessert.name], @potluck.menu[:desserts]
+    assert_equal [@meatballs.name, @pork.name], @potluck.menu[:entres]
+    assert_equal [@dip.name, @salad.name, @pizza.name], @potluck.menu[:appetizers]
   end
 
   def test_it_exists
@@ -25,5 +43,28 @@ class CardTest < Minitest::Test
     assert_equal 2, @potluck.dishes.length
     assert_equal @salad, @potluck.dishes[0]
     assert_equal @meatballs, @potluck.dishes[1]
+  end
+
+  def test_get_all_from_category
+    @potluck.add_dish(@salad)
+    @potluck.add_dish(@pizza)
+    @potluck.add_dish(@pork)
+    @potluck.add_dish(@meatballs)
+    @potluck.add_dish(@dessert)
+    assert_equal 5, @potluck.dishes.length
+    assert_equal 2, @potluck.get_all_from_category(:entre).length
+    assert_instance_of Array, @potluck.get_all_from_category(:appetizer)
+    assert_instance_of Dish, @potluck.get_all_from_category(:appetizer)[0]
+    assert_equal @salad, @potluck.get_all_from_category(:appetizer)[0]
+  end
+
+  def test_ratio
+    @potluck.add_dish(@salad)
+    @potluck.add_dish(@pizza)
+    @potluck.add_dish(@pork)
+    @potluck.add_dish(@meatballs)
+    @potluck.add_dish(@dessert)
+    @potluck.add_dish(@dip)
+    assert_equal 50.0, @potluck.ratio(:appetizer)
   end
 end
