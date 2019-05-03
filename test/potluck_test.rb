58e2832c2c8a60252b550b4bf2ac37715dc4bbeb
Jake Miller <jake0miller@gmail.com>
2019-05-03 10:25:05 -0600

    Initialize dishes array

--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -5,11 +5,15 @@ require './lib/potluck'
 
 class CardTest < Minitest::Test
   def setup
-    #@dish = Dish.new("Couscous Salad", :appetizer)
+    @dish = Dish.new("Couscous Salad", :appetizer)
     @potluck = Potluck.new("7-13-18")
   end
 
   def test_it_exists
     assert_instance_of Potluck, @potluck
+  end
+
+  def test_dishes_starts_empty
+    assert_empty @potluck.dishes
   end
 end
