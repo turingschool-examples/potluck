4947fdabec584cb2f18b92b1902ea33b33e0b0de
Ezekiel Clark <clarkzeke@gmail.com>
2019-12-02 10:55:45 -0700

    Finish get all from category test

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -10,4 +10,15 @@ class Potluck
   def add_dish(dish)
     @dishes << dish
   end
+
+  def get_all_from_category(category)
+    category_list = []
+
+    @dishes.each do |dish|
+      if dish.category == category
+        category_list << dish
+      end
+    end 
+    category_list
+  end
 end
--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -5,19 +5,34 @@ require './lib/potluck'
 
 class PotluckTest < Minitest::Test
 
-  def test_it_exists
-    potluck = Potluck.new("7-13-18")
+  def setup
+    @potluck = Potluck.new("7-13-18")
+    @couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    @cocktail_meatballs = Dish.new("Cocktail Meatballs", :entree)
+    @summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    @roast_pork = Dish.new("Roast Pork", :entree)
+    @candy_salad = Dish.new("Candy Salad", :dessert)
+  end
 
-    assert_instance_of Potluck, potluck
+  def test_it_exists
+    assert_instance_of Potluck, @potluck
   end
 
   def test_potluck_dishes
-    potluck = Potluck.new("7-13-18")
-    couscous_salad = Dish.new("Couscous Salad", :appetizer)
-    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entree)
+    # require "pry"; binding.pry
+    @potluck.add_dish(@couscous_salad)
+    @potluck.add_dish(@cocktail_meatballs)
+    # require "pry"; binding.pry
+    assert_equal [@couscous_salad,@cocktail_meatballs], @potluck.dishes
+  end
+
+  def test_get_all_from_category
+    @potluck.add_dish(@couscous_salad)
+    @potluck.add_dish(@summer_pizza)
+    @potluck.add_dish(@roast_pork)
+    @potluck.add_dish(@cocktail_meatballs)
+    @potluck.add_dish(@candy_salad)
     require "pry"; binding.pry
-    potluck.add_dish(couscous_salad)
-    potluck.add_dish(cocktail_meatballs)
-    require "pry"; binding.pry
+    assert_equal [@couscous_salad,@summer_pizza], @potluck.get_all_from_category(:appetizer)
   end
 end
