1a395b784f3263ea05e1e27d0142a5f4e67b03ce
Ezekiel Clark <clarkzeke@gmail.com>
2019-12-02 10:34:10 -0700

    Finish test potluck dishes and edit test name for dish

---
+++ test/potluck_test.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/dish'
+require './lib/potluck'
+
+class PotluckTest < Minitest::Test
+
+  def test_it_exists
+    potluck = Potluck.new("7-13-18")
+
+    assert_instance_of Potluck, potluck
+  end
+
+  def test_potluck_dishes
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entree)
+    require "pry"; binding.pry
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(cocktail_meatballs)
+    require "pry"; binding.pry
+  end
+end
