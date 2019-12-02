395b0051f3a60c12fb957c53b71106cd349a45da
Ezekiel Clark <clarkzeke@gmail.com>
2019-12-02 10:12:28 -0700

    Finish dish test and dish

--- test/dish_test.rb
+++ test/dish_test.rb
@@ -1,2 +1,12 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/dish'
+
+class TurnTest < Minitest::Test
+
+  def test_it_exists
+    dish = Dish.new("Couscous Salad", :appetizer)
+
+    assert_instance_of Dish, dish
+  end
+end 
