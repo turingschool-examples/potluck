3140b1f6c848124cbe433fc6387a4a4160807724
David Atkins <atki1080@gmail.com>
2019-12-02 10:11:58 -0700

    Add potluck

---
+++ lib/potluck.rb
@@ -0,0 +1,10 @@
+class Potluck
+  attr_reader :date, :dishes
+
+  def initialize(date)
+    @date = date
+    @dishes = []
+  end
+
+  
+end
---
+++ test/potluck_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/potluck'
+
+class PotluckTest < Minitest::Test
+  def test_it_exists
+    potluck = Potluck.new("7-13-18")
+    assert_instance_of Potluck, potluck
+  end
+
+  def test_instance_variables
+    potluck = Potluck.new("7-13-18")
+    assert_equal "7-13-18", potluck.date
+    assert_equal [], potluck.dishes
+  end
+end
