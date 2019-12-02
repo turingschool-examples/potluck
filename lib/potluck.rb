674fe92105498d5633bf739904e7361b51b17295
Ezekiel Clark <clarkzeke@gmail.com>
2019-12-02 11:52:18 -0700

    Finish menu and ratio but not associated tests

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -18,7 +18,47 @@ class Potluck
       if dish.category == category
         category_list << dish
       end
-    end 
+    end
     category_list
   end
+
+  def menu
+    appetizer_list = []
+
+    @dishes.each do |dish|
+      if dish.category == :appetizer
+        appetizer_list << dish.name
+      end
+    end
+
+    entree_list = []
+
+    @dishes.each do |dish|
+      if dish.category == :entree
+        entree_list << dish.name
+      end
+    end
+
+    dessert_list = []
+
+    @dishes.each do |dish|
+      if dish.category == :dessert
+        dessert_list << dish.name
+      end
+    end
+
+    menu = { :appetizer => appetizer_list.sort, :entree => entree_list.sort, :dessert => dessert_list.sort}
+  end
+
+  def ratio(category)
+    category_list = []
+
+    @dishes.each do |dish|
+      if dish.category == category
+        category_list << dish
+      end
+    end
+    (100.0 * category_list.length) / @dishes.length
+  end 
+
 end
--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -12,6 +12,7 @@ class PotluckTest < Minitest::Test
     @summer_pizza = Dish.new("Summer Pizza", :appetizer)
     @roast_pork = Dish.new("Roast Pork", :entree)
     @candy_salad = Dish.new("Candy Salad", :dessert)
+    @bean_dip = Dish.new("Bean Dip", :appetizer)
   end
 
   def test_it_exists
@@ -32,7 +33,30 @@ class PotluckTest < Minitest::Test
     @potluck.add_dish(@roast_pork)
     @potluck.add_dish(@cocktail_meatballs)
     @potluck.add_dish(@candy_salad)
-    require "pry"; binding.pry
+    # require "pry"; binding.pry
     assert_equal [@couscous_salad,@summer_pizza], @potluck.get_all_from_category(:appetizer)
+  end
+
+  def test_menu
+    @potluck.add_dish(@couscous_salad)
+    @potluck.add_dish(@summer_pizza)
+    @potluck.add_dish(@roast_pork)
+    @potluck.add_dish(@cocktail_meatballs)
+    @potluck.add_dish(@candy_salad)
+    @potluck.add_dish(@bean_dip)
+
+    @potluck.menu
+
+    require "pry"; binding.pry
+
+    #iterate over the dish names and shovel them into a an array category, then sort that array
+    #then assign that array to a hash named the category
+    #then shovel the hash into a menu array
+    # assert_equal @potluck.dishes.sort, @potluck.menu
+
+  end
+
+  def test_ratio
+
   end
 end
