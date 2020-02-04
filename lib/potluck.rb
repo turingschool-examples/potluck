9b358875dce99935bacab447ad84f83a144ae21e
Alex Lynch <mlynch5187@gmail.com>
2020-02-03 21:25:18 -0700

    Complete iteration_3

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -9,4 +9,7 @@ class Potluck
     @dishes << dish
   end
 
+  def get_all_from_category(category)
+    dishes.find_all {|dish| dish.category == category}
+  end
 end
--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -7,6 +7,12 @@ class PotluckTest < MiniTest::Test
 
   def setup
     @potluck = Potluck.new("7-13-18")
+    @couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    @cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    @summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    @roast_pork = Dish.new("Roast Pork", :entre)
+    @cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    @candy_salad = Dish.new("Candy Salad", :dessert)
   end
 
   def test_it_exists
@@ -22,18 +28,28 @@ class PotluckTest < MiniTest::Test
   end
 
   def test_dishes_can_be_added_to_potluck
-    @couscous_salad = Dish.new("Couscous Salad", :appetizer)
-    @cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
     @potluck.add_dish(@couscous_salad)
     @potluck.add_dish(@cocktail_meatballs)
     assert_equal [@couscous_salad, @cocktail_meatballs], @potluck.dishes
   end
 
   def test_dishes_can_be_counted
-    @couscous_salad = Dish.new("Couscous Salad", :appetizer)
-    @cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
     @potluck.add_dish(@couscous_salad)
     @potluck.add_dish(@cocktail_meatballs)
     assert_equal 2, @potluck.dishes.length
   end
+
+  def test_potluck_dishes_can_be_retrieved_by_category
+    @potluck.add_dish(@couscous_salad)
+    @potluck.add_dish(@summer_pizza)
+    @potluck.add_dish(@roast_pork)
+    @potluck.add_dish(@cocktail_meatballs)
+    @potluck.add_dish(@candy_salad)
+    assert_equal [@couscous_salad, @summer_pizza], @potluck.get_all_from_category(:appetizer)
+    assert_equal @couscous_salad, @potluck.get_all_from_category(:appetizer).first
+    assert_equal "Couscous Salad", @potluck.get_all_from_category(:appetizer).first.name
+  end
 end
+# pry(main)> potluck.get_all_from_category(:appetizer).first.name
+# => "Couscous Salad"
+# ```
