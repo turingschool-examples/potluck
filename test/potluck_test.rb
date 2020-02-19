f12cc459eee977e37658766f77f9a4eabd7e014c
Ashkan Abbasi <airashkan@yahoo.com>
2020-02-19 15:55:39 -0700

    Create test Potluck has attributes

--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -9,9 +9,13 @@ class PotluckTest < Minitest::Test
 
   def test_it_exists
     potluck = Potluck.new("7-13-18")
-
     assert_instance_of Potluck, potluck
+  end
 
+  def test_it_has_attributes
+    potluck = Potluck.new("7-13-18")
+    assert_equal "7-13-18", potluck.date
+    assert_equal [], potluck.dishes
   end
 
 end
