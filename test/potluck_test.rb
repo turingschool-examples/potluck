b4b42a5f5bd2f337836a7302ccad572e5796e564
Ashkan Abbasi <airashkan@yahoo.com>
2020-02-19 15:43:43 -0700

    Create Potluck test and test it exists

---
+++ test/potluck_test.rb
@@ -0,0 +1,17 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/dish'
+require './lib/potluck'
+
+
+class PotluckTest < Minitest::Test
+
+  def test_it_exists
+    potluck = Potluck.new("7-13-18")
+
+    assert_instance_of Potluck, potluck
+
+  end
+
+end
