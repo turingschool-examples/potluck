9fc0fd23153625ebb1cadc9feb579c4c077fe695
Ashkan Abbasi <airashkan@yahoo.com>
2020-02-19 16:49:30 -0700

    Finish Test it can add dishes

--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -18,4 +18,16 @@ class PotluckTest < Minitest::Test
     assert_equal [], potluck.dishes
   end
 
+  def test_it_can_add_dishes
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(cocktail_meatballs)
+    assert_equal Array, potluck.dishes.class
+    assert_equal [couscous_salad, cocktail_meatballs], potluck.dishes
+    assert_equal 2, potluck.dishes.length
+
+  end
+
 end
