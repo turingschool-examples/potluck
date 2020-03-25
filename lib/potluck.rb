947df3a05f6527d034125afd3343b8208cd734d6
Nicola Kim <nicola.copsey@gmail.com>
2020-03-24 20:12:58 -0700

    add updated test to potluck_test and Potluck class

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -1,8 +1,13 @@
 class Potluck
+  attr_reader :date, :dishes
 
   def initialize(date)
     @date = date
     @dishes = []
   end
+
+  #def next_method
+    #code
+  #end
 
 end
--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -8,8 +8,18 @@ class PotluckTest < Minitest::Test
 
   def test_it_exists
     potluck = Potluck.new("7-13-18")
-
     assert_instance_of Potluck, potluck
   end
+
+  def test_it_has_date
+    potluck = Potluck.new("7-13-18")
+    assert_equal "7-13-18", potluck.date
+  end
+
+  # def test_it_can_add_dishes
+  #   couscous_salad = Dish.new("Couscous Salad", :appetizer)
+  #   cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+  #
+  # end
 
 end
