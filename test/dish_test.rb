3610f85876ca56fe2d9d8cba0c3de3e320261566
Daniel Halverson <daniel.halverson@hotmail.com>
2020-05-11 10:14:49 -0600

    "Adding dish and dish testing"

---
+++ test/dish_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/dish'
+
+class DishTest < Minitest::Test
+
+  def test_it_exists
+  dish = Dish.new("Couscous Salad", :appetizer)
+
+    assert_instance_of Dish, dish
+  end
+
+
+end
