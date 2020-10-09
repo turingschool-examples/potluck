c946e2332b5b06bc436ae332adcc4d2850308515
Ely Hess <ely.hess@me.com>
2020-10-09 08:12:25 -0600

    Add Dish class

---
+++ test/dish_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/dish'
+
+
+class Test < Minitest::Test
+
+  def setup
+    @dish = Dish.new("Couscous Salad", :appetizer)
+  end
+
+  def test_it_exists_and_has_attributes
+
+    assert_instance_of Dish, @dish
+    assert_equal 'Couscous Salad', @dish.name
+    assert_equal :appetizer, @dish.category
+  end
+
+end
