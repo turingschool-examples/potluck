f63c9c33c69fc87a43ad152681a8decc73c5957a
Phil McCarthy <philmcc2631@gmail.com>
2020-10-11 11:34:27 -0600

    Add method to pass iteration 3

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -8,4 +8,10 @@ class Potluck
   def add_dish(dish)
     @dishes << dish
   end
+
+  def get_all_from_category(category)
+    @dishes.select do |dish|
+      dish.category == category
+    end
+  end
 end
