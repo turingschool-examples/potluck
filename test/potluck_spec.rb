4205194ebcd1ef428aa715c488e33b1af9a596a0
Matt Kragen <mattdkragen@gmail.com>
2021-05-17 11:24:09 -0700

    Create category test

--- test/potluck_spec.rb
+++ test/potluck_spec.rb
@@ -32,8 +32,31 @@ RSpec.describe Potluck do
         summer_pizza = Dish.new("Summer Pizza", :appetizer)
         roast_pork = Dish.new("Roast Pork", :entre)
         cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+        candy_salad = Dish.new("Candy Salad", :dessert)
         potluck.add_dish(couscous_salad)
 
         expect(potluck.dishes).to eq([couscous_salad])
+
+        potluck.add_dish(summer_pizza)
+        potluck.add_dish(roast_pork)
+        potluck.add_dish(cocktail_meatballs)
+        potluck.add_dish(candy_salad)
+        expect(potluck.dishes).to eq([couscous_salad, summer_pizza, roast_pork, cocktail_meatballs, candy_salad])
+    end
+
+    it 'retrieves dishes from category' do
+        potluck = Potluck.new("7-13-18")
+        couscous_salad = Dish.new("Couscous Salad", :appetizer)
+        summer_pizza = Dish.new("Summer Pizza", :appetizer)
+        roast_pork = Dish.new("Roast Pork", :entre)
+        cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+        candy_salad = Dish.new("Candy Salad", :dessert)
+        potluck.add_dish(couscous_salad)
+        potluck.add_dish(summer_pizza)
+        potluck.add_dish(roast_pork)
+        potluck.add_dish(cocktail_meatballs)
+        potluck.add_dish(candy_salad)
+
+        expect(potluck.get_all_from_category(:appetizer)).to eq([couscous_salad, summer_pizza])
     end
 end
