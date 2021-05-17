bd9e342bf1687a187978fc45656aa5722301c4b0
Matt Kragen <mattdkragen@gmail.com>
2021-05-17 11:13:12 -0700

    Create add dish test

--- test/potluck_spec.rb
+++ test/potluck_spec.rb
@@ -15,13 +15,25 @@ RSpec.describe Potluck do
     end
 
     it 'holds dishes' do
+        potluck = Potluck.new("7-13-18")
         couscous_salad = Dish.new("Couscous Salad", :appetizer)
         cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
-        potluck = Potluck.new("7-13-18")
         potluck.dishes << couscous_salad
         potluck.dishes << cocktail_meatballs
 
         expect(potluck.dishes[0]).to eq(couscous_salad)
         expect(potluck.dishes[1]).to eq(cocktail_meatballs)
+        expect(potluck.dishes.length).to eq(2)
+    end
+
+    it 'adds dishes' do
+        potluck = Potluck.new("7-13-18")
+        couscous_salad = Dish.new("Couscous Salad", :appetizer)
+        summer_pizza = Dish.new("Summer Pizza", :appetizer)
+        roast_pork = Dish.new("Roast Pork", :entre)
+        cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+        potluck.add_dish(couscous_salad)
+
+        expect(potluck.dishes).to eq(couscous_salad)
     end
 end
