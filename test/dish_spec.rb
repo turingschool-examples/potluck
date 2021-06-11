42cea2f7956682c2eda8dd9351984bfae64fa432
Matt Kragen <mattdkragen@gmail.com>
2021-06-10 20:20:29 -0700

    clean up with RuboCop

--- test/dish_spec.rb
+++ test/dish_spec.rb
@@ -1,21 +1,21 @@
 require './lib/dish'
 
 RSpec.describe Dish do
-    it 'exists' do
-        dish = Dish.new('Couscous Salad', :appetizer)
+  it 'exists' do
+    dish = Dish.new('Couscous Salad', :appetizer)
 
-        expect(dish).to be_an_instance_of(Dish)
-    end
+    expect(dish).to be_an_instance_of(Dish)
+  end
 
-    it 'returns name' do
-        dish = Dish.new("Couscous Salad", :appetizer)
+  it 'returns name' do
+    dish = Dish.new('Couscous Salad', :appetizer)
 
-        expect(dish.name).to eq("Couscous Salad")
-    end
+    expect(dish.name).to eq('Couscous Salad')
+  end
 
-    it 'returns category' do
-        dish = Dish.new("Couscous Salad", :appetizer)
+  it 'returns category' do
+    dish = Dish.new('Couscous Salad', :appetizer)
 
-        expect(dish.category).to eq(:appetizer)
-    end
+    expect(dish.category).to eq(:appetizer)
+  end
 end
