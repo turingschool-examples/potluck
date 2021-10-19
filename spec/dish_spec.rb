ae2223b5833c70f1e45fe30637162cf8ebd34fcb
David Kassin <david.j.kassin@gmail.com>
2021-10-19 17:30:23 -0600

    test: Return name and category argument

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -5,4 +5,17 @@ describe Dish do
     dish = Dish.new("Couscous Salad", :appetizer)
     expect(dish).to be_a Dish
   end
+
+  it 'returns dish name argument' do
+    dish = Dish.new("Couscous Salad", :appetizer)
+    expect(dish.name).to eq ("Couscous Salad")
+  end
+
+  it 'returns category name argument' do
+    dish = Dish.new("Couscous Salad", :appetizer)
+    expect(dish.category).to eq (:appetizer)
+  end
+
+
+
 end
