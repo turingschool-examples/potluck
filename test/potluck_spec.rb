f06ad535cd2f7c641fd70513a2a8eb14a7097c0e
Gauri Joshi <gauri728@gmail.com>
2022-05-17 21:10:49 -0700

    Test/dishes length is equal to number of dishes

--- test/potluck_spec.rb
+++ test/potluck_spec.rb
@@ -39,4 +39,13 @@ RSpec.describe Potluck do
 
   end
 
+  it "has a length equal to the number of dishes" do
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    potluck = Potluck.new("7-13-18")
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(cocktail_meatballs)
+    expect(potluck.dishes.length).to eq(2)
+  end
+
 end
