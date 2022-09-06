74552949a351bf6ac449c0e067dce29a1fe68a4f
Rich Kaht <adamselene1776@protonmail.com>
2022-09-06 12:34:01 -0600

    Add method to get all dishes from a class

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -8,4 +8,12 @@ class Potluck
   def add_dish(dish)
     @dishes << dish
   end
+  
+  def get_all_from_category(dish_category)
+    category = []
+    @dishes.map do |dish|
+      category << dish if dish.category == dish_category
+    end
+    return category
+  end
 end
--- test/potluck_spec.rb
+++ test/potluck_spec.rb
@@ -27,4 +27,21 @@ RSpec.describe Potluck do
     expect(potluck.dishes).to eq([couscous_salad, cocktail_meatballs])
     expect(potluck.dishes.length).to eq(2)
   end
+  
+  it "#get_all_from_category returns an array of dishes from that category" do
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+    # require "pry"; binding.pry
+    expect(potluck.get_all_from_category(:appetizer)).to eq([couscous_salad, summer_pizza])
+  end
 end
