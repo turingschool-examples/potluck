5f0886b9e891712508c43469bdf1c95a62d82add
Rich Kaht <adamselene1776@protonmail.com>
2022-09-07 19:14:30 -0600

    Feat/Test: Add more methods

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -16,4 +16,28 @@ class Potluck
     end
     return category
   end
+  
+  def menu
+    full_menu = {
+      appetizers: [],
+      entres: [],
+      desserts: []
+    }
+    sorted = @dishes.sort_by {|dish| dish.name}
+    sorted.each do |dish|
+      if dish.category == :appetizer
+        full_menu[:appetizers] << dish.name
+      elsif dish.category == :entre
+        full_menu[:entres] << dish.name
+      elsif dish.category == :dessert
+        full_menu[:desserts] << dish.name
+      end
+    end
+    full_menu
+  end
+  
+  def ratio(course)
+    course_num = @dishes.select {|dish| dish.category == course}
+    (course_num.size.to_f / @dishes.size * 100).round(1)
+  end
 end
--- test/potluck_spec.rb
+++ test/potluck_spec.rb
@@ -41,7 +41,58 @@ RSpec.describe Potluck do
     potluck.add_dish(roast_pork)
     potluck.add_dish(cocktail_meatballs)
     potluck.add_dish(candy_salad)
-    # require "pry"; binding.pry
     expect(potluck.get_all_from_category(:appetizer)).to eq([couscous_salad, summer_pizza])
   end
+  
+  it "#menu returns hash of dishes by category" do
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    bean_dip = Dish.new("Bean Dip", :appetizer)
+    
+    full_menu = {
+      appetizers: ['Bean Dip', 'Couscous Salad', 'Summer Pizza'],
+      entres: ['Cocktail Meatballs', 'Roast Pork'],
+      desserts: ['Candy Salad']
+    }
+      
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+    potluck.add_dish(bean_dip)
+    # require "pry"; binding.pry
+    expect(potluck.menu).to eq(full_menu)
+  end
+  
+  it "#ratio returns the ratio of the course" do
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    bean_dip = Dish.new("Bean Dip", :appetizer)
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+    potluck.add_dish(bean_dip)
+    
+    expect(potluck.ratio(:dessert)).to eq((1/6.to_f * 100).round(1))
+    expect(potluck.ratio(:appetizer)).to eq((3/6.to_f * 100).round(1))
+  end
+  
+  
+  
+  
+  
+  
+  
+  
 end
